13f3147bf504c9b126d257af3d3d02019e98ce6a
lzw <993096281@qq.com>
2019-06-27 23:55:44 -0400

    test

---
+++ test_sh/fedora_matrixkv_test_4value.sh
@@ -0,0 +1,96 @@
+#! /bin/sh
+
+value_array=(1024 4096 16384 65536)
+test_all_size=8192000000   #8G
+
+
+bench_db_path="/home/hm/ceshi"
+wal_dir="/pmem/log"
+bench_value="4096"
+bench_compression="none" #"snappy,none"
+
+#bench_benchmarks="fillseq,stats,readseq,readrandom,stats" #"fillrandom,fillseq,readseq,readrandom,stats"
+#bench_benchmarks="fillrandom,stats,readseq,readrandom,readrandom,readrandom,stats"
+bench_benchmarks="fillrandom,stats,wait,stats,readseq,readrandom,readrandom,readrandom,stats"
+#bench_benchmarks="fillseq,stats"
+bench_num="20000000"
+bench_readnum="100000"
+#bench_max_open_files="1000"
+max_background_jobs="3"
+max_bytes_for_level_base="`expr 8 \* 1024 \* 1024 \* 1024`" 
+#max_bytes_for_level_base="`expr 256 \* 1024 \* 1024`" 
+
+pmem_path="/pmem/nvm"
+use_nvm="true"
+
+bench_file_path="$(dirname $PWD )/db_bench"
+
+bench_file_dir="$(dirname $PWD )"
+
+if [ ! -f "${bench_file_path}" ];then
+bench_file_path="$PWD/db_bench"
+bench_file_dir="$PWD"
+fi
+
+if [ ! -f "${bench_file_path}" ];then
+echo "Error:${bench_file_path} or $(dirname $PWD )/db_bench not find!"
+exit 1
+fi
+
+RUN_ONE_TEST() {
+    const_params="
+    --db=$bench_db_path \
+    --wal_dir=$wal_dir \
+    --value_size=$bench_value \
+    --benchmarks=$bench_benchmarks \
+    --num=$bench_num \
+    --reads=$bench_readnum \
+    --compression_type=$bench_compression \
+    --max_background_jobs=$max_background_jobs \
+    --max_bytes_for_level_base=$max_bytes_for_level_base \
+    --use_nvm_module=$use_nvm \
+    --reset_nvm_storage=$use_nvm \
+    --pmem_path=$pmem_path \
+    "
+    cmd="$bench_file_path $const_params >>out.out 2>&1"
+    echo $cmd >out.out
+    echo $cmd
+    eval $cmd
+}
+
+CLEAN_CACHE() {
+    rm -f $bench_db_path/*
+    sleep 2
+    sync
+    echo 3 > /proc/sys/vm/drop_caches
+    sleep 2
+}
+
+COPY_OUT_FILE(){
+    mkdir $bench_file_dir/result > /dev/null 2>&1
+    res_dir=$bench_file_dir/result/value-$bench_value
+    mkdir $res_dir > /dev/null 2>&1
+    \cp -f $bench_file_dir/compaction.csv $res_dir/
+    \cp -f $bench_file_dir/OP_DATA $res_dir/
+    \cp -f $bench_file_dir/OP_TIME.csv $res_dir/
+    \cp -f $bench_file_dir/out.out $res_dir/
+    \cp -f $bench_file_dir/NVM_LOG $res_dir/
+    \cp -f $bench_db_path/OPTIONS-* $res_dir/
+    \cp -f $bench_db_path/LOG $res_dir/
+}
+RUN_ALL_TEST() {
+    for value in ${value_array[@]}; do
+        CLEAN_CACHE
+        bench_value="$value"
+        bench_num="`expr $test_all_size / $bench_value`"
+
+        RUN_ONE_TEST
+        if [ $? -ne 0 ];then
+            exit 1
+        fi
+        COPY_OUT_FILE
+        sleep 5
+    done
+}
+
+RUN_ALL_TEST
